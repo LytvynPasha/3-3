7282a0e3986aa8d36d359d17b2928a89ac422797
blck f0x <lytvyn.pavlo@gmail.com>
2021-10-06 16:59:48 +0300

    Добавьте файлы проекта.

---
+++ 3,3.cpp
@@ -0,0 +1,32 @@
+﻿// Lab_03_3.cpp
+// < Литвин Павло Андрійович >
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 14
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+	double y; // результат обчислення виразу
+	cout << "x = "; cin >> x;
+	// розгалуження в повній формі
+	if (x <= -4)
+		y = -2;
+	else
+		if (-4 <= x && x <= 0)
+			y = x/4;
+		else
+			if (0 < x && x <= 1)
+				y = x;
+			else
+				if (2 < x && x <= 2)
+					y = 3*x-2;
+				else
+					y = ( - 4 * x + 40) / 8;
+	cout << endl;
+	cout << "y = " << y << endl;
+	cin.get();
+	return 0;
+}
